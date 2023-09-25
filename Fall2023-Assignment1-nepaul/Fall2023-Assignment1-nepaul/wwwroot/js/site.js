40db978d1e6faa417decac86a80d2484f444153d
NATE_LAPTOP\natep <nepaul@crimson.ua.edu>
2023-09-24 23:54:24 -0500

    main changes -NP

--- Fall2023-Assignment1-nepaul/Fall2023-Assignment1-nepaul/wwwroot/js/site.js
+++ Fall2023-Assignment1-nepaul/Fall2023-Assignment1-nepaul/wwwroot/js/site.js
@@ -1,4 +1,13 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿function openCity(evt, tabName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("city");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < x.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " w3-red";
+}
